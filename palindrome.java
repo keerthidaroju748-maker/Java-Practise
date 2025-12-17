65f3d598ccd5d98220431002ec3fd2fddb934d76
keerthidaroju748-maker <keerthidaroju748@gmail.com>
2025-12-17 10:24:02 +0530

    Palindrome

---
+++ palindrome.java
@@ -0,0 +1,14 @@
+public class palindrome {
+public static void main(String[] args) {
+        String original = "civic";
+        StringBuilder sb = new StringBuilder(original);
+        
+        if (original.equals(sb.reverse().toString())) {
+            System.out.println("Palindrome");
+        }
+         else {
+            System.out.println("Not Palindrome");
+         }
+    }
+}
+
