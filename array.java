c8cb256301c1452dd16f4bc53ff589ad3dbb6d69
keerthidaroju748-maker <keerthidaroju748@gmail.com>
2025-12-17 09:37:49 +0530

    array index

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static void main(String[] args) {
+String[] names ={"chinni","ammu","sathu","aqsa","siddhu","sanju","keerthi","sathvika","chinnari","pintu","bintu"};
+
+System.out.println("current first name:");
+System.out.println(names[6]);
+System.out.println("updating the first name:");
+names[6] = "Siriii";
+System.out.println(names[6]);
+}
+}
